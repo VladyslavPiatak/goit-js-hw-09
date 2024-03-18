1662694920efa3d789a2ee266cd751dcfd765d7f
VladyslavPiatak <92726974+VladyslavPiatak@users.noreply.github.com>
2024-03-18 12:55:16 +0000

    Deploying to gh-pages from @ VladyslavPiatak/goit-js-hw-09@ad953029227f7c3ecda0db2cf2d8c65e3226bd15 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector(".feedback-form"),a="feedback-form-state";e.addEventListener("input",m);function m(n){const t={email:e.elements.email.value.trim(),message:e.elements.message.value.trim()};localStorage.setItem(a,JSON.stringify(t))}
+//# sourceMappingURL=commonHelpers2.js.map
